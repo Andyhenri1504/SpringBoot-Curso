fc364b1b651bf2a17036c1d09974e0783b9c4bd2
Andyhenri1504 <andrew.henrique.servilheri@gmail.com>
2025-08-19 19:23:18 -0300

    Um CRUD basico

--- produtosapi/src/main/java/com/cursospring/produtosapi/controller/ProdutoController.java
+++ produtosapi/src/main/java/com/cursospring/produtosapi/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import com.cursospring.produtosapi.model.Produto;
 import com.cursospring.produtosapi.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -45,6 +46,11 @@ public class ProdutoController {
     public void atualizar(@PathVariable("id") String id, @RequestBody Produto produto){
         produto.setId(id);
         produtoRepository.save(produto);
+    }
+
+    @GetMapping
+    public List<Produto> buscar(@RequestParam("nome") String nome){
+         return produtoRepository.findByNome(nome);
     }
 
 }
